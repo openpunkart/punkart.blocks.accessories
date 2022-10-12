fb788958162aa914c0304029e338273cb972bd7c
Gerald Bauer <gerald.bauer@gmail.com>
2022-10-12 16:39:11 +0200

    up

---
+++ sandbox/prepare.rb
@@ -0,0 +1,53 @@
+####
+#  to run use:
+#   $   ruby sandbox/prepare.rb
+
+
+require 'pixelart'
+
+
+palettes = [
+  'ye_olde_punks',
+  'dr_ellis_monk'
+]
+
+
+buf = String.new('')
+
+palettes.each do |palette|
+
+  path = "./#{palette}.csv"
+
+  puts "==> reading #{path}..."
+  recs = read_csv( path )
+  puts "  #{recs.size} record(s)"
+
+  buf << "## #{palette} (#{recs.size} skin tones)\n\n"
+
+  recs.each do |rec|
+    hex        = rec['color']
+    name       = rec['name']
+    more_names =  (rec['more_names'] || '').split( '|' )
+
+    ## normalize spaces in more names
+    names = [name] + more_names
+    names = names.map {|str| str.strip.gsub(/[ ]{2,}/, ' ' )}
+
+    id  = "#{palette}-#{name}"
+    bar = Image.new( 256, 32, hex )
+    bar.save( "./tmp/#{id}.png" )
+
+    color = Color.parse( hex )
+
+    buf << "- ![](i/#{id}.png}) #{Color.format( color )} -- #{names.join( ' • ' )}\n"
+  end
+  buf << "\n\n"
+end
+
+
+puts buf
+
+write_text( "./tmp/page.md",  buf )
+
+
+puts "bye"
